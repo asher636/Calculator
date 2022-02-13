e1c36c9f0fe9f4139ef1ff63e47aee8b269f7559
asher636 <asherahsan636@gmail.com>
2022-02-13 20:38:39 +0100

    setting up calculator project

---
+++ RS_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RS_Calculator
+//
+//  Created by Shornali on 13.02.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
